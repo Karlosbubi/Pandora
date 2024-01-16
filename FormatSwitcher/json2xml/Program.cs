ac4f14cc7ae74f02a303f7acb4dbd2d75f082c5b
Kurt Brunner <Kontakt@Karlosbubi.de>
2024-01-16 20:55:26 +0100

    init (with format switcher)

---
+++ FormatSwitcher/json2xml/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+if (args.Length < 1) {
+    Console.Error.WriteLine("No files provided");
+    return;
+}
+
+foreach (string jsonFilePath in args)
+{
+    try
+    {
+        string jsonText = File.ReadAllText(jsonFilePath);
+        XmlDocument xmlDocument = JsonConvert.DeserializeXmlNode(jsonText) ?? throw new ArgumentNullException();
+
+        string newXmlFilePath = jsonFilePath.Replace(".json", ".xml");
+        string xmlDeclaration = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
+
+        string xmlText = xmlDeclaration + "\n" + PrettyXml(xmlDocument.OuterXml);
+        
+        File.WriteAllText(newXmlFilePath, xmlText);
+    }
+    catch (Exception e)
+    {
+        Console.Error.WriteLine($"Skipped file \"{jsonFilePath}\" due to Error {e.Message} ");
+    }
+}
+
+return;
+
+
+static string PrettyXml(string xml)
+{
+    var stringBuilder = new StringBuilder();
+
+    var element = XElement.Parse(xml);
+
+    var settings = new XmlWriterSettings
+    {
+        OmitXmlDeclaration = true, // Would add UTF-16
+        Indent = true,
+        NewLineOnAttributes = true
+    };
+
+    using (var xmlWriter = XmlWriter.Create(stringBuilder, settings))
+    {
+        element.Save(xmlWriter);
+    }
+
+    return stringBuilder.ToString();
+}
---
+++ FormatSwitcher/xml2json/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml;
+using Newtonsoft.Json;
+using Formatting = Newtonsoft.Json.Formatting;
+
+if (args.Length < 1) {
+    Console.Error.WriteLine("No files provided");
+    return;
+}
+
+foreach (string xmlFilePath in args)
+{
+    try
+    {
+        XmlDocument xmlDocument = new XmlDocument();
+        xmlDocument.Load(xmlFilePath);
+        foreach (XmlNode node in xmlDocument)
+        {
+            if (node.NodeType == XmlNodeType.XmlDeclaration)
+            {
+                xmlDocument.RemoveChild(node);
+            }
+        }
+        string jsonText = JsonConvert.SerializeXmlNode(xmlDocument, Formatting.Indented);
+        
+        string newJsonFilePath = xmlFilePath.Replace(".xml", ".json");
+        File.WriteAllText(newJsonFilePath, jsonText);
+    }
+    catch (Exception e)
+    {
+        Console.Error.WriteLine($"Skipped file \"{xmlFilePath}\" due to Error {e.Message} ");
+    }
+}
